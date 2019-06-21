dd25ea87be7ff819e26e6136edd1ae196112f246
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-06-21 01:07:48 -0500

    Fix

--- train.py
+++ train.py
@@ -147,8 +147,8 @@ def train():
             # vgg_loss = 2e-6 * tl.cost.mean_squared_error(feature_fake, feature_real, is_mean=True)
             g_loss = mse_loss + g_gan_loss
         grad = tape.gradient(g_loss, G.trainable_weights)
-        grad = tape.gradient(d_loss, D.trainable_weights)
         g_optimizer.apply_gradients(zip(grad, G.trainable_weights))
+        grad = tape.gradient(d_loss, D.trainable_weights)
         d_optimizer.apply_gradients(zip(grad, D.trainable_weights))
         del tape
         step += 1
